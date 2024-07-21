401475d0fe58322e06a32087e5446602abb433bd
Dangerous875 <crbarzabal@gmail.com>
2024-07-21 00:01:20 -0300

    practice v2

--- app/src/main/java/com/example/searchdogspracticecompose/MainActivity.kt
+++ app/src/main/java/com/example/searchdogspracticecompose/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // instanciar dagger y permisos internet
             // navController NavHost configurar navegacion
 
         }
--- app/src/main/java/com/example/searchdogspracticecompose/SearchDogsApp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.searchdogspracticecompose
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SearchDogsApp : Application() {
-}
